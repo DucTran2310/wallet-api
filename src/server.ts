25df01b6e1f84aca170a9ce1e7868177803f2e42
DucTran2310 <trananhduc23102000@gmail.com>
2025-06-04 11:13:20 +0700

    Add script cron

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,15 @@ dotenv.config();
 
 import app from "./app";
 import { initDB } from "./config/db";
+import job from './config/cron'
+
+if (process.env.NODE_ENV === 'production') job.start()
 
 const PORT = process.env.PORT || 5001;
+
+app.get('api/heath', (req, res) => {
+  res.status(200).json({status: "ok"})
+})
 
 initDB().then(() => {
   app.listen(PORT, () => {
